79fa3f8bc45a261b4d552b15a4a45a4701209844
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-15 15:09:47 +0100

    Create project

---
+++ BarChartInteractive/BarChartInteractive/BarChartInteractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartInteractiveApp.swift
+//  BarChartInteractive
+//
+//  Created by Jesus Antonio Gil on 15/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BarChartInteractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
